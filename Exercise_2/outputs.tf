01c722de32ca71601030f687e1302ffa34dae3c4
HoaiEm <hoaiem.heli22@gmail.com>
2024-05-25 04:14:06 +0700

    [feat] Submit project

--- Exercise_2/outputs.tf
+++ Exercise_2/outputs.tf
@@ -1,1 +1,3 @@
-# TODO: Define the output variable for the lambda function.
+output "lambda_function_invoke_arn" {
+  value = aws_lambda_function.greet_lambda.invoke_arn
+}
--- Exercise_2/variables.tf
+++ Exercise_2/variables.tf
@@ -1,1 +1,13 @@
-# TODO: Define the variable for aws_region
+variable "aws_access_key" {
+  default = ""
+}
+variable "aws_secret_key" {
+  default = ""
+}
+variable "aws_region" {
+  default = "us-east-1"
+}
+
+variable "lambda_function_name" {
+  default = "greet_lambda"
+}
